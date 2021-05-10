2938816f422140666e4d88e1857601394dc3b2ad
Takahashi <megane.nt5mf@gmail.com>
2021-05-11 01:36:00 +0900

    残り時間を表示する機能を追加

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float time; // 1playの秒数
+    public Text timerText;
+    int seconds;
+
 
     void Update(){
         time -= Time.deltaTime;
+        seconds = (int)time; // intにキャスト
+        timerText.text = seconds.ToString(); // Stringにキャストして表示
+
         if(time <= 0){ // time up後リザルト画面へ
             SceneManager.LoadScene("result");
         }
