f16ca4fe0d8e15836627847c1328128c73b08937
Takahashi <megane.nt5mf@gmail.com>
2021-06-11 14:23:11 +0900

    リザルト画面で獲得スコアを表示

---
+++ Assets/Scripts/ResultShow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultShow : MonoBehaviour
+{
+    public Text Score;
+    
+    void Start()
+    {
+        Score.text = GameManeger.totalScore.ToString() + " 点";
+    }
+}
