80e1e613c140f2bf2f446c0f1302a717fea9e415
Takahashi <megane.nt5mf@gmail.com>
2021-06-16 12:46:05 +0900

    プレイ開始ボタンをタッチで遷移するように変更

--- Assets/Scripts/PlayStart.cs
+++ Assets/Scripts/PlayStart.cs
@@ -7,7 +7,17 @@ public class PlayStart : MonoBehaviour
 {
     public string sceneName;
 
-    void OnMouseDown(){
-        SceneManager.LoadScene(sceneName);
+    void Update()
+    {
+        foreach (Touch t in Input.touches)
+        {
+            if(t.phase == TouchPhase.Began){ // タッチされた瞬間に
+                var ray = Camera.main.ScreenToWorldPoint(t.position); // タッチの位置からレイを飛ばす
+                var hit = Physics2D.Raycast(ray, Vector2.zero); // 2D用のレイキャスト（ぶつかったオブジェクトの情報とか持ってる）
+                if(hit.collider.gameObject.name ==this.name){ // ぶつかったオブジェクトの名前が自分と同じだったら
+                    SceneManager.LoadScene(sceneName); // シーン遷移
+                }
+            }
+        }
     }
 }
